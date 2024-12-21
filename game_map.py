50b421763d8a2d59bfd53340f2c6a7b27cb22ef0
Andrew Ignácio <andrewignacio@outlook.com.br>
2024-12-21 12:43:59 -0300

    feat: add FOV

--- game_map.py
+++ game_map.py
@@ -9,9 +9,15 @@ class GameMap:
     self.height = height
 
     self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
+    self.visible = np.full((width, height), fill_value=False, order="F")
+    self.explored = np.full((width, height), fill_value=False, order="F")
 
   def in_bounds(self, x: int, y: int):
     return 0 <= x < self.width and 0 <= y < self.height
   
   def render(self, console: Console):
-    console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+    console.rgb[0:self.width, 0:self.height] = np.select(
+      condlist=[self.visible, self.explored],
+      choicelist=[self.tiles["light"], self.tiles["dark"]],
+      default=tile_types.UNEXPLORED
+    )
--- tile_types.py
+++ tile_types.py
@@ -14,7 +14,8 @@ tile_dt = np.dtype(
   [
     ("walkable", np.bool),
     ("transparent", np.bool), # Block FOV
-    ("dark", graphics_dt) # Graphics for when tile is not in FOV
+    ("dark", graphics_dt), # Graphics for when tile is not in FOV
+    ("light", graphics_dt) # Graphics for when tile is in FOV
   ]
 )
 
@@ -22,18 +23,23 @@ def new_tile(
     *, # Parameter order doesn't matter
     walkable: int,
     transparent: int,
-    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
+    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
   ) -> np.ndarray:
-  return np.array((walkable, transparent, dark), dtype=tile_dt)
+  return np.array((walkable, transparent, dark, light), dtype=tile_dt)
+
+UNEXPLORED = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphics_dt)
 
 floor = new_tile(
   walkable=True,
   transparent=True,
-  dark=(ord(" "), (255,255,255), (50,50,150))
+  dark=(ord(" "), (255,255,255), (50,50,150)),
+  light=(ord(" "), (255,255,255), (200,180,50))
 )
 
 wall = new_tile(
   walkable=False,
   transparent=False,
-  dark=(ord(" "), (255,255,255), (0,0,100))
+  dark=(ord(" "), (255,255,255), (0,0,100)),
+  light=(ord(" "), (255,255,255), (130,110,50))
 )
