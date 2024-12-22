6896046a59302da46669182290ae1dfd4c2e366c
Andrew Ignácio <andrewignacio@outlook.com.br>
2024-12-22 14:12:27 -0300

    feat: add enemy AI

--- game_map.py
+++ game_map.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from typing import Iterable, Optional, TYPE_CHECKING
+from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 
 import numpy as np
 from tcod.console import Console
 
+from entity import Actor
 import tile_types
 
 if TYPE_CHECKING:
@@ -25,10 +26,24 @@ class GameMap:
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
 
+    @property
+    def actors(self) -> Iterator[Actor]:
+        yield from (
+            entity
+            for entity in self.entities
+            if isinstance(entity, Actor) and entity.is_alive
+        )
+
     def get_blocking_entity(self, x: int, y: int) -> Optional[Entity]:
         for entity in self.entities:
             if entity.block_movement and entity.x == x and entity.y == y:
                 return entity
+        return None
+
+    def get_actor_at(self, x: int, y: int) -> Optional[Actor]:
+        for actor in self.actors:
+            if actor.x == x and actor.y == y:
+                return actor
         return None
 
     def in_bounds(self, x: int, y: int):
