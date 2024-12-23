09f4e32e9c80848e8ce93099b5d768ae60a5caf5
Andrew Ignácio <andrewignacio@outlook.com.br>
2024-12-23 20:52:04 -0300

    feat: add entity description at mouse position

--- engine.py
+++ engine.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
@@ -10,7 +9,7 @@ from game_map import GameMap
 from input_handlers import MainGameEventHandler
 from log import Log
 
-import ui
+from ui import render_names_at_mouse_position, render_ui
 
 if TYPE_CHECKING:
     from entity import Actor
@@ -25,6 +24,7 @@ class Engine:
         self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
         self.log = Log()
+        self.mouse_position = (0, 0)
 
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
@@ -40,9 +40,8 @@ class Engine:
 
         self.game_map.explored |= self.game_map.visible
 
-    def render(self, console: Console, context: Context) -> None:
+    def render(self, console: Console) -> None:
         self.game_map.render(console)
-        self.log.render(console, 42, 45, 40, 5)
-        ui.render_ui(console, self.player)
-        context.present(console)
-        console.clear()
+        self.log.render(console, 20, 45, 40, 5)
+        render_ui(console, self.player)
+        render_names_at_mouse_position(console=console, x=20, y=44, engine=self)
--- ui.py
+++ ui.py
@@ -2,29 +2,52 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import colors
-
 from entity import Actor
 
+import colors
+
 if TYPE_CHECKING:
-    from tcod import Console
+    from tcod.console import Console
+    from engine import Engine
+    from game_map import GameMap
+
+
+def get_names_at_position(x: int, y: int, game_map: GameMap) -> str:
+    if not game_map.in_bounds(x, y) or not game_map.visible[x, y]:
+        return ""
+
+    names = ",".join(
+        entity.name for entity in game_map.entities if entity.x == x and entity.y == y
+    )
+
+    return names.capitalize()
 
 
 def render_ui(console: Console, player: Actor):
     console.print(
         x=1,
-        y=46,
-        string=f"HP:  {player.fighter.hp}\{player.fighter.max_hp}",
+        y=44,
+        string=f"HP:  {player.fighter.hp}/{player.fighter.max_hp}",
     )
 
     console.print(
         x=1,
-        y=47,
+        y=45,
         string=f"DEF: {player.fighter.power}",
     )
 
     console.print(
         x=1,
-        y=48,
+        y=46,
         string=f"POW: {player.fighter.power}",
     )
+
+
+def render_names_at_mouse_position(
+    console: Console, x: int, y: int, engine: Engine
+) -> None:
+    mouse_x, mouse_y = engine.mouse_position
+
+    names_at_mouse_position = get_names_at_position(mouse_x, mouse_y, engine.game_map)
+
+    console.print(x=x, y=y, string=names_at_mouse_position, fg=colors.pointing_at_log)
